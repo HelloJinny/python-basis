394687194acf6b471cca7909ceca68086d0d12fc
HelloJinny <vkfkdto0209@naver.com>
2021-05-31 00:26:11 +0900

    SQLite 연동, 테이블 생성, 데이터 삽입

--- section12-1.py
+++ section12-1.py
@@ -26,3 +26,46 @@ sqlite3.version :  2.6.0
 sqlite3.sqlite_version 3.35.4
 """
 
+# DB생성 & Autocommit
+# 본인 DB 파일 경로
+conn = sqlite3.connect('C:/workspaces/Git/python-basis/resource/database.db', isolation_level=None)
+
+# DB생성(메모리)
+# conn = sqlite3.connect(":memory:")
+
+# Cursor연결
+c = conn.cursor()
+print('Cursor Type : ', type(c))
+
+"""
+Cursor Type :  <class 'sqlite3.Cursor'>
+"""
+
+# 테이블 생성(Datatype : TEXT NUMERIC INTEGER REAL BLOB)
+c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username text, email text, phone text, website text, regdate text)")  # AUTOINCREMENT
+
+# 데이터 삽입
+c.execute("INSERT INTO users VALUES (1 ,'Kim','Kim@naver.com', '010-0000-0000', 'Kim.com', ?)", (nowDatetime,))
+c.execute("INSERT INTO users(id, username, email, phone, website, regdate) VALUES (?, ?, ?, ?, ?, ?)", (2, 'Park', 'Park@naver.com', '010-1111-1111', 'Park.com', nowDatetime))
+
+# Many 삽입(튜플, 리스트) ✨
+userList = (
+    (3, 'Lee', 'Lee@naver.com', '010-2222-2222', 'Lee.com', nowDatetime),
+    (4, 'Cho', 'Cho@naver.com', '010-3333-3333', 'Cho.com', nowDatetime),
+    (5, 'Yoo', 'Yoo@naver.com', '010-4444-4444', 'Yoo.com', nowDatetime)
+)
+c.executemany("INSERT INTO users(id, username, email, phone, website, regdate) VALUES (?, ?, ?, ?, ?, ?)", userList)
+
+
+# 테이블 데이터 삭제
+# conn.execute("delete from users")
+# print("users db deleted : ", conn.execute("delete from users").rowcount, "rows")
+
+# 커밋 : isolation_level=None 일 경우 자동 반영(Auto Commit)
+# conn.commit()
+
+# 롤백
+# conn.rollback()
+
+# 접속 해제
+conn.close()
